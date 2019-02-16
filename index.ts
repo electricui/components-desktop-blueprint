58479811803ef55c669a586b6ab16fe02d24478f
Michael <kill.coder@gmail.com>
2019-02-16 13:58:07 +1030

    chore: Minor cleanup

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './src/Button'
 export { default as Slider } from './src/Slider'
 export { default as Checkbox } from './src/Checkbox'
+export { default as RadioGroup } from './src/RadioGroup'
