050b2d75a426d30af68d92e9169b842a736c4544
Michael Orenstein <michael@whiteshadows.me>
2019-05-27 14:42:52 +0930

    feat: DisconnectionModal component in the template

---
+++ src/DisconnectionModal/index.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode } from 'react'
+import { Alert, Intent, IconName } from '@blueprintjs/core'
+import {
+  useDeviceHasAcceptableConnection,
+  useDeviceDisconnect,
+} from '@electricui/components-core'
+
+type DisconnectionModalProps = {
+  /** A function that navigates to the connections screen. */
+  navigateToConnectionsScreen: () => void
+  /** Text to display in the button that goes to the connections screen without disconnecting. */
+  backText: string
+  /** Text to display in the button that goes to the connections screen, disconnecting from the device. */
+  disconnectText: string
+  /** The icon to display. */
+  icon?: IconName
+  /** The intent of the modal. */
+  intent?: Intent
+  /** The contents to render in the modal. */
+  children?: ReactNode
+}
+
+const DisconnectionModal = ({
+  navigateToConnectionsScreen,
+  backText,
+  disconnectText,
+  intent,
+  icon,
+  children,
+}: DisconnectionModalProps) => {
+  const disconnect = useDeviceDisconnect()
+  const hasAcceptableConnection = useDeviceHasAcceptableConnection()
+
+  return (
+    <Alert
+      cancelButtonText={backText}
+      confirmButtonText={disconnectText}
+      icon={icon}
+      intent={intent}
+      isOpen={!hasAcceptableConnection}
+      onCancel={navigateToConnectionsScreen}
+      onConfirm={() => {
+        navigateToConnectionsScreen()
+        disconnect()
+      }}
+    >
+      {children}
+    </Alert>
+  )
+}
+
+DisconnectionModal.defaultProps = {
+  backText: 'Go to the Connection Screen',
+  disconnectText: 'Disconnect',
+  children: (
+    <p>
+      Connection has been lost with your device. If we successfully reconnect
+      this dialog will be dismissed.
+    </p>
+  ),
+}
+
+export default DisconnectionModal
