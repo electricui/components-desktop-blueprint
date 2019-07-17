460b06f0c8fe1fa1cc2b26f4d91df642799e0f29
Scott Rapson <scottapotamas@gmail.com>
2019-07-17 19:15:25 +0930

    feat: isSubset utility

--- src/utils.ts
+++ src/utils.ts
@@ -20,31 +20,3 @@ export function isElementOfType<P = {}>(
     element.type.displayName === ComponentType.displayName
   )
 }
-
-type ComparisonSet = {
-  [key: string]: any
-}
-
-export function isSubset(superset: ComparisonSet, subset: ComparisonSet) {
-  if (
-    typeof superset !== 'object' ||
-    superset === null ||
-    (typeof subset !== 'object' || subset === null)
-  ) {
-    return false
-  }
-
-  for (const key of Object.keys(subset)) {
-    const subsetItem = subset[key]
-    const supersetItem = superset[key]
-    if (
-      typeof subsetItem === 'object' && subsetItem !== null
-        ? !isSubset(supersetItem, subsetItem)
-        : supersetItem !== subsetItem
-    ) {
-      return false
-    }
-  }
-
-  return true
-}
