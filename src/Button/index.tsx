876a437cbac5752864f526d759a6867e118e311a
Michael Orenstein <michael@whiteshadows.me>
2019-08-10 18:22:19 +0930

    feat: Buttons accept an onClick and can be disabled

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -9,23 +9,29 @@ import {
   InjectedElectricityProps,
   StateTree,
 } from '@electricui/components-core'
+import { CALL_CALLBACK } from '@electricui/core'
 
 /**
  * Remove the IButtonProps ones we don't want to show in the documentation
  * @remove type
- * @remove onClick
  * @remove loading
  * @remove elementRef
  */
 interface ElectricButtonProps extends IButtonProps {
   /** A function that returns a StateTree or just a StateTree to write to the device upon clicking */
-  writer: (() => StateTree) | StateTree
+  writer?: (() => StateTree) | StateTree
+
+  /** A callback messageID to call upon clicking */
+  callback?: string
 
   /** Action text. Can be any single React renderable. */
   children?: ReactNode
 
   /** Whether to not request acknowledgement of packets sent upon clicking. */
   noAck?: boolean
+
+  /** If the button should display a disabled state and not send output. */
+  disabled?: boolean
 }
 
 /**
@@ -46,6 +52,10 @@ class ElectricButton extends Component<
   onClick = () => {
     const { write, writer, noAck } = this.props
 
+    if (!writer) {
+      return
+    }
+
     if (typeof writer === 'function') {
       write(writer(), !noAck)
       return
@@ -60,10 +70,28 @@ class ElectricButton extends Component<
     write(writer, !noAck)
   }
 
+  allOnClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    const { write, onClick, callback, noAck, disabled } = this.props
+
+    if (disabled) { 
+      return 
+    }
+
+    this.onClick()
+
+    if (callback) {
+      write({ [callback]: CALL_CALLBACK }, !noAck)
+    }
+
+    if (typeof onClick === 'function') {
+      onClick(event)
+    }
+  }
+
   render() {
     const rest = removeElectricProps(this.props, ['writer', 'noAck'])
 
-    return <Button onClick={this.onClick} {...rest} />
+    return <Button onClick={this.allOnClick} {...rest} />
   }
 }
 
